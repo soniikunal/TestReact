a6619a034db06661790eaf89dc50c716f75790ef
kunal soni <kunal94611@gmail.com>
2023-12-17 19:48:44 +0530

    Navbar changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import { Navbar } from './Components/common/Navbar.jsx'
-// import Footer from './Components/MUI/Footer.jsx'
-// import Navbar from './Components/MUI/AppBar.jsx'
-import MiniDrawer from './Components/MUI/Sidebar.jsx'
+import MiniDrawer from './Components/MUI/Navbar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Navbar /> */}
-    {/* <Navbar/> */}
     <MiniDrawer AppComponent={<App />} />
     {/* <App /> */}
-    {/* <Footer /> */}
   </React.StrictMode>,
 )
