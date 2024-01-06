2782d1e4ea44ee2ab31886d5c52506072e6fd3bc
Himanshu Soni <sonihimanshu073@gmail.com>
2024-01-06 11:59:37 +0530

    created ATDquestions and model files

---
+++ src/Components/common/AddATDQuestionModal.jsx
@@ -0,0 +1,255 @@
+import React, { useState, useEffect } from 'react'
+import { Container } from '@mui/material'
+import { Sheet, FormControl, FormLabel, Modal, ModalClose, AspectRatio, Typography, Input, FormHelperText, Select, Option, RadioGroup, Radio, Divider, Box, SvgIcon, Button, styled, ModalOverflow, IconButton } from '@mui/joy';
+import { Close } from '@mui/icons-material';
+import { AddATDQuestion, GetCategories } from '../../config/apiConfig.js'
+import TinyMCEEditor from './TinyMCEEditor.jsx';
+
+
+const AddATDQuestionModal = () => {
+
+    const [open, setOpen] = useState(false);
+    const [avatar, setAvatar] = useState(null);
+    const [image, setImage] = useState(null);
+    const [correctOpt, setcorrectOpt] = useState(null);
+    const [optionQuantity, setOptionQuantity] = useState(0);
+    const [options, setOptions] = useState([]);
+    const [question, setQuestion] = useState('');
+    const [category, setCategory] = useState('');
+    const [categoryOptions, setCategoryOptions] = useState([]);
+
+    const VisuallyHiddenInput = styled('input')`
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    height: 1px;
+    overflow: hidden;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    white-space: nowrap;
+    width: 1px;
+    `;
+
+    const setTheAvatar = (e) => {
+        const reader = new FileReader(),
+            files = e.target.files
+        reader.onload = function () {
+            setImage(reader.result)
+        }
+        setAvatar(files[0])
+        reader.readAsDataURL(files[0])
+    }
+    const removeAvatar = () => {
+        setAvatar(null)
+        setImage(null)
+    }
+    const handleOptionQuantityChange = (event) => {
+        if (event?.target) {
+            const quantity = parseInt(event.target.innerHTML, 10);
+            setOptionQuantity(quantity);
+
+            // Create an array of empty strings to represent the initial state of options
+            const initialOptions = Array.from({ length: quantity }, () => '');
+            setOptions(initialOptions);
+        }
+    };
+    const fetchCategories = async () => {
+        try {
+            const allCategories = await GetCategories();
+            setCategoryOptions(allCategories)
+        } catch (error) {
+            console.error('FEtch failed:', error.message);
+        }
+    }
+    const handleCorrectRadio = (e) => {
+        if (e?.target) {
+            setcorrectOpt(parseInt(e.target.value))
+        }
+    }
+
+    const handleCategoryChange = (e) => {
+        if (e?.target) {
+            setCategory(e.target.innerHTML)
+        }
+    }
+
+    const handleOptionChange = (index, value) => {
+        const newOptions = [...options];
+        newOptions[index] = value;
+        setOptions(newOptions);
+    };
+
+    const handleQuestionChange = (e) => {
+        if (e?.target) {
+            setQuestion(e.target.value)
+        }
+    };
+
+    const handleAddQuestion = async () => {
+        // const formData = new FormData();
+        // formData.append('avatar', avatar);
+        // formData.append('question', question);
+        // formData.append('category', category);
+        // formData.append('options', options);
+        // formData.append('correctOpt', correctOpt);
+
+        const formData = {
+            avatar,
+            question,
+            category,
+            options,
+            correctOpt
+        }
+        try {
+            const result = await AddATDQuestion(formData)
+            console.log('Question added successfully:', result);
+        } catch (error) {
+            console.error('Failed to add question:', error.message);
+        }
+    };
+
+    return (
+        <>
+            <Button variant="outlined" color="neutral" onClick={() => {
+                fetchCategories()
+                setOpen(true)
+            }}>
+                Add Questions
+            </Button>
+            <Modal
+                aria-labelledby="modal-title"
+                aria-describedby="modal-desc"
+                open={open}
+                onClose={() => setOpen(false)}
+                // sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', overflow:'auto' }}
+                sx={{
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start'
+                }}
+            >
+                <ModalOverflow>
+                    <Sheet
+                        variant="outlined"
+                        sx={{
+                            maxWidth: 1000,
+                            width: 900,
+                            borderRadius: 'md',
+                            p: 3,
+                            boxShadow: 'lg',
+                            margin: 'auto',
+                            flex: 'none'
+                        }}
+                    >
+                        <ModalClose variant="plain" sx={{ m: 1 }} />
+                        <Typography
+                            component="h2"
+                            id="modal-title"
+                            level="h4"
+                            textColor="inherit"
+                            fontWeight="lg"
+                            mb={1}
+                        >
+                            Add Question
+                        </Typography>
+                        <Sheet variant="outlined" color="neutral" sx={{ p: 4 }}>
+                            <FormControl sx={{ mb: 2 }}>
+                                <FormLabel>Question</FormLabel>
+                                <Input placeholder="Question" variant="soft" onChange={e => handleQuestionChange(e)} />
+                                {/* <TinyMCEEditor /> */}
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Category</FormLabel>
+                                <Select
+                                    placeholder="Category"
+                                    variant="soft"
+                                    onChange={e => handleCategoryChange(e)}
+                                    value={category}
+                                >
+                                    {categoryOptions.map((option) => (
+                                        <Option key={option.name} value={option.name}>{option.name}</Option>))}
+                                </Select>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Number of Options</FormLabel>
+                                <Select
+                                    placeholder="No. of Options"
+                                    variant="soft"
+                                    value={optionQuantity} onChange={(e) => handleOptionQuantityChange(e)}
+                                    sx={{ mb: 2 }}
+                                >
+                                    {[...Array(3).keys()].map((num) => (
+                                        <Option key={num + 2} value={num + 2}>
+                                            {num + 2}
+                                        </Option>
+                                    ))}
+                                </Select>
+                                <Divider />
+                                {options.map((option, index) => (
+                                    <Input startDecorator={`${index + 1}.`} placeholder={`Option ${index + 1}`} key={index} variant="soft" sx={{ my: 1 }} onChange={(e) => handleOptionChange(index, e.target.value)} />
+                                ))}
+                            </FormControl>
+                            <Divider />
+                            {options.length > 0 && (
+                                <FormControl>
+                                    <FormLabel id="optionRadio">Correct Option</FormLabel>
+                                    <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+                                        {options.map((option, index) => (
+                                            <Radio value={index} key={index} label={index + 1} checked={correctOpt === index } onChange={handleCorrectRadio} color="success" sx={{ mr: 2 }} />
+                                        ))}
+                                    </Box>
+                                </FormControl>)}
+                            <FormControl>
+                                <FormLabel>Image</FormLabel>
+                                {avatar && (
+                                    <Box sx={{
+                                        display: 'contents'
+                                    }}>
+                                        <img src={image} style={{ maxHeight: '250px', objectFit: 'contain' }} />
+                                        <IconButton aria-label="close" onClick={removeAvatar} sx={{ position: 'absolute', top: '0', right: '0px', }}>
+                                            <Close />
+                                        </IconButton>
+                                    </Box>
+                                )}
+                                <Button
+                                    component="label"
+                                    role={undefined}
+                                    tabIndex={-1}
+                                    variant="outlined"
+                                    color="neutral"
+                                    startDecorator={
+                                        <SvgIcon>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth={1.5}
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                                                />
+                                            </svg>
+                                        </SvgIcon>
+                                    }
+                                >
+                                    Upload a file
+                                    {/* <VisuallyHiddenInput type="file" onChange={(e) => saveImage(e)} /> */}
+                                    <VisuallyHiddenInput type="file" onChange={(e) => setTheAvatar(e)} />
+                                </Button>
+                            </FormControl>
+                            <FormControl>
+                                <Button onClick={handleAddQuestion}>Add Question</Button>
+                            </FormControl>
+
+                        </Sheet>
+                    </Sheet>
+                </ModalOverflow>
+            </Modal>
+        </>
+    )
+}
+
+export default AddATDQuestionModal
---
+++ src/views/Admin/ATDQuestion.jsx
@@ -0,0 +1,231 @@
+import React, { useEffect, useState } from 'react'
+import { Card, Typography, Input, Stack, Box, Button } from '@mui/joy'
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import LayersIcon from '@mui/icons-material/Layers';
+import QuestionPreviewModal from '../../Components/common/QuestionPreviewModal'
+import AddATDQuestionModal from '../../Components/common/AddATDQuestionModal'
+import EditQuestionModal from '../../Components/common/EditQuestionModal'
+import Category from '../../Components/common/Category'
+import DeleteConfModal from '../../Components/common/DeleteConfModal';
+import { DelATDQuestion, GetATDQuestions } from '../../config/apiConfig';
+import { toastError, toastSuccess } from '../../Utils/Toasts';
+
+const data = [{
+    "_id": {
+        "$oid": "6586b0287fcba88007ce80b5"
+    },
+    "question": "The given statement contains few words/phrases printed in bold. Choose the correct option which is grammatically correct and conveys equivalent meaning for the bold text in the given statement.The scientist's groundbreaking research was a Pandora's box in the field of genetics.",
+    "options": [
+        "The research opened up new possibilities in genetics.",
+        "The research was a source of hope for genetic advancements.",
+        "The research had both positive and negative consequences in the field of genetics",
+        "The research was a universally accepted solution in genetics"
+    ],
+    "IsDescriptive": false,
+    "category": "Geography",
+    "uniqueCode": "5304",
+    "__v": 0
+},
+{
+    "_id": {
+        "$oid": "6586b02a7fcba88007ce80b7"
+    },
+    "question": "What is the capital of France?",
+    "options": [
+        "Berlin",
+        "London",
+        "Paris",
+        "Rome"
+    ],
+    "IsDescriptive": false,
+    "category": "Geography",
+    "uniqueCode": "3037",
+    "__v": 0
+}]
+
+const ATDQuestion = () => {
+    const [questions, setQuestions] = useState([]);
+    const [selectedQuestion, setSelectedQuestion] = useState(null);
+    const [deletedQuestion, setDeletedQuestion] = useState(null);
+    const [editQuestion, setEditQuestion] = useState(null);
+    const [isModalOpen, setModalOpen] = useState(true);
+    const [searchIpt, setSearchIpt] = useState("")
+
+    useEffect(() => {
+        fetchQuestion()
+    }, [])
+
+
+    const columns = [
+        { field: 'uniqueCode', headerName: 'ID', width: 100 },
+        {
+            field: 'question',
+            headerName: 'Question',
+            width: 1000,
+            editable: true,
+        },
+        {
+            field: 'category',
+            headerName: 'Category',
+            width: 200,
+            editable: true,
+        },
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: ({ id }) => {
+                return [
+                    <GridActionsCellItem
+                        icon={<LayersIcon />}
+                        label="Preview"
+                        className="textPrimary"
+                        onClick={handleShowClick(id)}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={handleDeleteClick(id)}
+                        color="inherit"
+                    />,
+                ];
+            },
+        },
+    ];
+
+    const handleInputChange = (event) => {
+        const {value} = event.target;
+        setSearchIpt(value);
+    }
+
+    const fetchQuestion = async () => {
+        try {
+            const allQuestions = await GetATDQuestions(searchIpt);
+            setQuestions(allQuestions)
+        } catch (error) {
+            console.error('FEtch failed:', error.message);
+        }
+    }
+
+    const handleDeleteClick = (id) => () => {
+        const selectedQue = questions.filter(e => e.uniqueCode == id)
+        setDeletedQuestion(selectedQue[0])
+        setModalOpen(true);
+    };
+
+    const handleEditClick = (id) => () => {
+        const editedQue = questions.filter(e => e.uniqueCode == id)
+        setEditQuestion(editedQue[0])
+        setModalOpen(true);
+    };
+
+    const onDelete = async () => {
+        try {
+            const { _id } = deletedQuestion
+            const deletedObj = await DelATDQuestion(_id)
+            fetchQuestion()
+            toastSuccess(deletedObj.message)
+            handleCloseModal()
+        } catch (error) {
+            toastError(error.message)
+            console.error('Update failed:', error.message);
+        }
+    };
+
+    const handleShowClick = (question) => () => {
+        const selectedQue = questions.filter(e => e.uniqueCode == question)
+        setSelectedQuestion(selectedQue[0]);
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+        fetchQuestion()
+        setDeletedQuestion(null)
+        setSelectedQuestion(null);
+        setEditQuestion(null);
+    };
+
+    return (
+        <>
+            <Card
+                color="primary"
+                size="md"
+                variant="soft"
+            >
+                <Card
+                    color="primary"
+                    invertedColors
+                    variant="solid"
+                    sx={{ display: 'flex' }}
+                >
+                    <Stack
+                        direction="row"
+                        justifyContent="flex-start"
+                        alignItems="center"
+                        spacing={2}
+                    >
+                        <Typography level="title-md">Search by:</Typography>
+                        <Input placeholder="Question" onChange={handleInputChange} value={searchIpt}/>
+                        <Button variant="solid" size="sm" onClick={fetchQuestion}>
+                            Search
+                        </Button>
+                        <Box style={{ marginLeft: 'auto' }}>
+                            <AddATDQuestionModal />
+                        </Box>
+                        <Category />
+                    </Stack>
+                </Card>
+
+                <DataGrid rows={questions}
+                    columns={columns}
+                    editMode="row"
+                    getRowId={(row) => row.uniqueCode}
+                    disableToolbar
+                />
+
+            </Card>
+            {selectedQuestion && (
+                <QuestionPreviewModal
+                    question={selectedQuestion}
+                    handleClose={handleCloseModal}
+                    open={isModalOpen}
+                />
+            )}
+            {
+                deletedQuestion && (
+                    <DeleteConfModal
+                        onDelete={onDelete}
+                        onClose={handleCloseModal}
+                        open={isModalOpen}
+                        deletedQuestion={deletedQuestion}
+                    />
+                )
+            }
+            {
+                editQuestion && (
+                    <EditQuestionModal
+                        onDelete={onDelete}
+                        onClose={handleCloseModal}
+                        open={isModalOpen}
+                        question={editQuestion}
+                    />
+                )
+            }
+        </>
+    )
+}
+
+export default ATDQuestion
